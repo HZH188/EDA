034db08ab62d517a406b3473251d1178b5f1b964
HZH188 <168643203+HZH188@users.noreply.github.com>
2024-06-25 20:28:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 using namespace std;
 using namespace std::chrono;
 
+//读取命令行中的三个参数
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         cerr << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
         return 1;
     }
 
+//第一个为输入文件，第二个为输出文件
+    
     string inputFile = argv[1];
     string outputFile = argv[2];
 
